41a2626c78eb36a8f6ef8b15a824482ff70755ad
RRmanky <indragusti089@gmail.com>
2025-12-03 14:29:59 +0700

    new

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -57,3 +57,12 @@ dependencies {
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
+dependencies {
+    // Networking
+    implementation 'com.squareup.retrofit2:retrofit:2.9.0'
+    implementation 'com.squareup.retrofit2:converter-gson:2.9.0'
+
+    // Coroutines untuk proses background
+    implementation 'org.jetbrains.kotlinx:kotlinx-coroutines-android:1.6.4'
+    implementation 'androidx.lifecycle:lifecycle-runtime-ktx:2.6.1'
+}
